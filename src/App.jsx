c416fe5c939af18f89b3deefcfe2349216d61783
UtopicRay <ray2021@gmail.com>
2024-04-25 14:57:01 -0400

    Music Audio Complete

--- src/App.jsx
+++ src/App.jsx
@@ -5,42 +5,62 @@ function App() {
     const [currentMusic, setCurrentMusic] = useState({
         songName: 'Full Moon Full Life',
         songArtist: 'Persona 3 Reload',
-        songSrc: '../public/music/Beneath-the-Mask-Persona-5.mp3',
+        songSrc: '../public/music/Azumi_Takahashi_Lotus_Juice_ATLUS_Sound_Team_ATLUS_GAME_MU.mp3',
         songAvatar: '../public/img/images-1.jpeg',
         songVideo: '../public/video/full-moon-full-life-full-version-lyrics-persona-3-reload_video_360p.mp4'
     })
     const currentAudio = useRef()
-    const [isPlayingAudio, setIsPlayingAudio] = useState(false)
+    const [isPlayingAudio, setIsPlayingAudio] = useState(true)
     const [musicIndex, setMusicIndex] = useState(0)
+    const [musicTotalLength, setMusicTotalLenght] = useState('04:54')
+    const [musicCurrentTime, setMusicCurrentTime] = useState('00:00')
     const musicAPI = [{
         songName: 'Full Moon Full Life',
         songArtist: 'Persona 3 Reload',
-        songSrc: '../public/music/Beneath-the-Mask-Persona-5.mp3',
-        songAvatar: '../public/img/images-1.jpeg',
-        songVideo: '../public/video/full-moon-full-life-full-version-lyrics-persona-3-reload_video_360p.mp4'
+        songSrc: '/public/music/Azumi_Takahashi_Lotus_Juice_ATLUS_Sound_Team_ATLUS_GAME_MU.mp3',
+        songAvatar: '/public/img/images-1.jpeg',
+        songVideo: '/public/video/full-moon-full-life-full-version-lyrics-persona-3-reload_video_360p.mp4'
     }, {
         songName: 'Color Of Your Night',
         songArtist: 'Persona 3 Reload',
-        songSrc: '../public/music/AL2.mp3',
-        songAvatar: '../public/img/images-2.jpeg',
-        songVideo: '../public/video/full-moon-full-life-full-version-lyrics-persona-3-reload_video_360p.mp4'
-    }]
+        songSrc: '/public/music/Lotus_Juice_Azumi_Takahashi_ATLUS_Sound_Team_ATLUS_GAME_MU.mp3',
+        songAvatar: '/public/img/images-2.jpg',
+        songVideo: '/public/video/Color-Your-Night-Persona-3-Reload.mp4'
+    },
+        {
+            songName: 'ROAD LESS TAKEN',
+            songArtist: 'Lyn/Lotus/Shihoko/Yumi',
+            songSrc: '/public/music/Yumi Kawamura_Shihoko Hirata_Lyn_Lotus Juice - ROAD LESS TAK.mp3',
+            songAvatar: '/public/img/persona-q2.webp',
+            songVideo: '/public/video/Color-Your-Night-Persona-3-Reload.mp4'
+        }]
     const handleAudio = () => {
         try {
             if (currentAudio.current.paused) {
                 currentAudio.current.play();
-                setIsPlayingAudio(!isPlayingAudio)
+                setIsPlayingAudio(true)
             } else {
                 currentAudio.current.pause();
 
-                setIsPlayingAudio(!isPlayingAudio)
+                setIsPlayingAudio(false)
             }
         } catch (error) {
             console.log(error)
         }
     }
+    const handlePreviusSong = () => {
+        if (musicIndex < musicAPI.length - 1) {
+            let setNumber = musicAPI.length - 1;
+            setMusicIndex(setNumber)
+            updateCurrentMusic(setNumber)
+        } else {
+            let setNumber = musicIndex - 1;
+            setMusicIndex(setNumber)
+            updateCurrentMusic(setNumber)
+        }
+    }
     const handleNextSong = () => {
-        if (musicIndex >= currentMusic.length - 1) {
+        if (musicIndex >= musicAPI.length - 1) {
             let setNumber = 0;
             setMusicIndex(setNumber)
             updateCurrentMusic(setNumber)
@@ -53,88 +73,103 @@ function App() {
     const updateCurrentMusic = (index) => {
         let musicObject = musicAPI[index]
         currentAudio.current.src = musicObject.songSrc
+        currentAudio.current.play()
         setCurrentMusic({
-            sonName: musicObject.songName,
+            songName: musicObject.songName,
             songArtist: musicObject.songArtist,
             songSrc: musicObject.songSrc,
             songVideo: musicObject.songVideo,
             songAvatar: musicObject.songAvatar
         })
-        setIsPlayingAudio(!isPlayingAudio)
-        currentAudio.current.play()
+
+        setIsPlayingAudio(true)
     }
-    const changeCurrentMusic = (e) => {
-        e.preventDefault()
-        setCurrentMusic()
-    }
-    const [audioProgress, setAudioProgress] = useState(60)
+
+    const [audioProgress, setAudioProgress] = useState(0)
     const handleMusicProgressBar = (e) => {
         setAudioProgress(e.target.value);
+        currentAudio.current.currentTime = e.target.value * currentAudio.current.duration / 100;
+    }
+    const handleAudioUpdate = () => {
+        //Update Minutes Total Lenght
+        let minutes = Math.floor(currentAudio.current.duration / 60);
+        let seconds = Math.floor(currentAudio.current.duration % 60);
+        let musicTotalLength1 = `${minutes < 10 ? (`0${minutes}`) : minutes} : ${seconds < 10 ? `0${seconds}` : seconds}`
+        setMusicTotalLenght(musicTotalLength1)
+        //Update Minutes Current Time
+        let CurrentMinutes = Math.floor(currentAudio.current.currentTime / 60);
+        let CurrentSeconds = Math.floor(currentAudio.current.currentTime % 60);
+        let currentMusic = `${CurrentMinutes < 10 ? (`0${CurrentMinutes}`) : CurrentMinutes} : ${CurrentSeconds < 10 ? `0${CurrentSeconds}` : seconds}`
+        setMusicCurrentTime(currentMusic)
+
+        const pogress = parseInt((currentAudio.current.currentTime / currentAudio.current.duration) * 100)
+        setAudioProgress(isNaN(pogress) ? 0 : pogress)
     }
     return (
-        <>
-            <div className='container'>
-                <audio src={currentMusic.songSrc} ref={currentAudio}></audio>
-                <video src={currentMusic.songVideo}
-                       autoPlay={true} muted={true} loop={true} className='backgroundVideo'></video>
-                <div className='blackScreen'></div>
-                <div className='music-container'>
-                    <p className='music-Player'>Music Player</p>
-                    <p className='music-Head-Name'>{currentMusic.songName}</p>
-                    <p className='music-Artist-Name'>{currentMusic.songArtist}</p>
-                    <img src={currentMusic.songAvatar} alt={currentMusic.songAvatar} className='songAvatar '/>
-                    <div className='musicTimeDiv'>
-                        <div className='currentTime'>00:00</div>
-                        <p className='musicTotalLenght'>03:49</p>
-                    </div>
-                    <input type='range' name='musicProgressBar' className='musicProgressBar' value={audioProgress}
-                           onChange={handleMusicProgressBar}/>
-                    <div className='musicControlers'>
+
+        <div className='container'>
+            <audio src={currentMusic.songSrc} ref={currentAudio} onEnded={handleNextSong}
+                   onTimeUpdate={handleAudioUpdate}></audio>
+            <video src={currentMusic.songVideo}
+                   autoPlay={true} muted={true} loop={true} className='backgroundVideo'></video>
+            <div className='blackScreen'></div>
+            <div className='music-container'>
+                <p className='music-Player'>Music Player</p>
+                <p className='music-Head-Name'>{currentMusic.songName}</p>
+                <p className='music-Artist-Name'>{currentMusic.songArtist}</p>
+                <img src={currentMusic.songAvatar} alt={currentMusic.songAvatar} className='songAvatar '/>
+                <div className='musicTimeDiv'>
+                    <div className='currentTime'>{musicCurrentTime}</div>
+                    <p className='musicTotalLenght'>{musicTotalLength}</p>
+                </div>
+                <input type='range' name='musicProgressBar' className='musicProgressBar' value={audioProgress}
+                       onChange={handleMusicProgressBar}/>
+                <div className='musicControlers'>
+                    <button className='musicBtn' onClick={handlePreviusSong}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                             fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                             stroke-linejoin="round"
+                             fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round"
+                             strokeLinejoin="round"
                              className="icon icon-tabler icons-tabler-outline icon-tabler-player-track-prev playBtn">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                             <path d="M21 5v14l-8 -7z"/>
                             <path d="M10 5v14l-8 -7z"/>
                         </svg>
+                    </button>
+                    <button className='musicBtn' onClick={handleAudio}>
                         {!isPlayingAudio ? (
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                 stroke-linejoin="round"
-                                 onClick={handleAudio}
-                                 className="playBtn icon icon-tabler icons-tabler-outline icon-tabler-circle-caret-right">
-                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                                <path d="M15 12l-4 -4v8z"/>
-                                <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0"/>
-                            </svg>) : (
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                 stroke-linejoin="round"
-                                 onClick={handleAudio}
+                                 fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round"
+                                 strokeLinejoin="round"
                                  className="icon icon-tabler icons-tabler-outline icon-tabler-player-pause playBtn">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                                 <path
                                     d="M6 5m0 1a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v12a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1z"/>
                                 <path
                                     d="M14 5m0 1a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v12a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1z"/>
+                            </svg>) : (
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                 fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round"
+                                 strokeLinejoin="round"
+                                 className="playBtn icon icon-tabler icons-tabler-outline icon-tabler-player-play">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <path d="M7 4v16l13 -8z"/>
                             </svg>)}
+                    </button>
 
-
+                    <button onClick={handleNextSong} className='musicBtn'>
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                              fill="none"
-                             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                             stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
                              className="playBtn icon icon-tabler icons-tabler-outline icon-tabler-player-track-next">
-                            onClick={handleNextSong}
                             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                             <path d="M3 5v14l8 -7z"/>
                             <path d="M14 5v14l8 -7z"/>
                         </svg>
-
-                    </div>
+                    </button>
                 </div>
             </div>
-        </>
+        </div>
+
     )
 }
 
