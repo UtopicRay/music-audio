a39a101edbb1ffb86f17891d93b5c36268cbe8bd
UtopicRay <ray2021@gmail.com>
2024-06-06 14:44:28 -0400

    'Add musicAPI'

---
+++ src/musicAPI.js
@@ -0,0 +1,21 @@
+ const musicAPI = [{
+    songName: 'Full Moon Full Life',
+    songArtist: 'Persona 3 Reload',
+    songSrc: '/public/music/Azumi_Takahashi_Lotus_Juice_ATLUS_Sound_Team_ATLUS_GAME_MU.mp3',
+    songAvatar: '/public/img/images-1.jpeg',
+    songVideo: '/public/video/full-moon-full-life-full-version-lyrics-persona-3-reload_video_360p.mp4'
+}, {
+    songName: 'Color Of Your Night',
+    songArtist: 'Persona 3 Reload',
+    songSrc: '/public/music/Lotus_Juice_Azumi_Takahashi_ATLUS_Sound_Team_ATLUS_GAME_MU.mp3',
+    songAvatar: '/public/img/images-2.jpg',
+    songVideo: '/public/video/Color-Your-Night-Persona-3-Reload.mp4'
+},
+    {
+        songName: 'ROAD LESS TAKEN',
+        songArtist: 'Lyn/Lotus/Shihoko/Yumi',
+        songSrc: '/public/music/Yumi Kawamura_Shihoko Hirata_Lyn_Lotus Juice - ROAD LESS TAK.mp3',
+        songAvatar: '/public/img/persona-q2.webp',
+        songVideo: '/public/video/Color-Your-Night-Persona-3-Reload.mp4'
+    }]
+export default musicAPI
